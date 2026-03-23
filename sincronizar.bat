971de3cede951d0cfc52841525e88cd59ae67c37
Lucas <lucas@example.com>
2026-03-23 14:34:50 -0300

    auto backup 23/03/2026 14:34:50,06

--- sincronizar.bat
+++ sincronizar.bat
@@ -1,14 +1,12 @@
 @echo off
-cd /d "C:\CAMINHO\DO\SEU\PROJETO"
+cd /d "C:\CAMINHO\DO\PROJETO"
 
 git add .
 
 git diff --cached --quiet
 if %errorlevel%==0 (
-    echo Nada para commitar.
+    echo Sem mudanças.
 ) else (
-    git commit -m "auto update"
+    git commit -m "auto backup %date% %time%"
     git push
 )
-
-pause
