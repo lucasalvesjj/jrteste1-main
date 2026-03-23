f7969f12a271c5d4177b594b6e9538df1ba9eb54
Lucas <lucas@example.com>
2026-03-23 18:46:08 -0300

    auto backup 23/03/2026 18:46:08,15

--- sincronizar.bat
+++ sincronizar.bat
@@ -3,9 +3,11 @@ cd /d "C:\Users\lucas\Downloads\jr1-main 2203\jrteste1-main"
 git checkout master
 git add .
 git diff --cached --quiet
-if %errorlevel%==0 (echo Sem mudanças.) else (git commit -m "auto backup %date% %time%"& git push origin master)
+if %errorlevel%==0 (echo Sem mudanças.) else (
+    git commit -m "auto backup %date% %time%"
+    git push origin master
+)
 
-REM CRIA E SINCRONIZA A BRANCH MAIN NO SEGUNDO REPO
 git remote | findstr /C:"seguinte" >nul || git remote add seguinte https://github.com/lucasalvesjj/comercial-jr-2.git
 git checkout -b main
 git push seguinte main:main --force
