93d3249e8569e2a0952cad065737ef7c4a64e8cc
Lucas <lucas@example.com>
2026-04-08 10:39:35 -0300

    auto backup 08/04/2026 10:39:35,44

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ import { htaccessPlugin } from "./vite-plugin-htaccess";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  esbuild: {
+    drop: mode === 'production' ? ['console', 'debugger'] : [],
+  },
   server: {
     host: "0.0.0.0",
     port: 8080,
